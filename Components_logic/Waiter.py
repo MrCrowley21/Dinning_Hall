4e0256d202283c520d57e4146cc45f157fb144c0
MrCrowley21 <graur.lenka@gmail.com>
2022-09-20 17:07:42 +0300

    Setting order generation

--- Components_logic/Waiter.py
+++ Components_logic/Waiter.py
@@ -1,5 +1,5 @@
 import logging
-from time import sleep
+from time import sleep, time
 import random
 from threading import Lock
 import requests
@@ -21,18 +21,21 @@ class Waiter:
                 # picking up the orders if table is ready
                 table.lock_order_state.acquire()
                 if table.state == waiting_to_make_an_order:
-                    order = self.get_the_order(table)
+                    table.generate_order(self.waiter_id)
+                    pick_up_time = time()
+                    order = self.get_the_order(table, pick_up_time)
                     self.send_order(table, order)
                 else:
                     table.lock_order_state.release()
 
     # simulate waiters actions toward a new table
-    def get_the_order(self, table):
+    def get_the_order(self, table, pick_up_time):
         # change state of table in waiting
         table.state = waiting_for_an_order_to_be_served
         table.lock_order_state.release()
         sleep(time_unit * random.randint(2, 4))  # set time for picking up the order
         order = table.pick_up_the_order()
+        order.pick_up_time = pick_up_time
         return order
 
     # notify the kitchen about new order arrival
@@ -40,6 +43,7 @@ class Waiter:
         # notify about order details
         logging.info(f'Waiter {self.waiter_id} is picking up the order from table {table.table_id} '
                      f'order {order.order_id}')
-        requests.post(f'{kitchen_container_url}receive_order', json=order.__dict__)
+        requests.post(f'{kitchen_url}receive_order', json=order.__dict__)
         # notify about successful request to the kitchen
-        logging.info(f'Order {order.order_id} has been sent to the Kitchen')
+        logging.info(f'Order {order.order_id} with the following structure:\n'
+                     f'{order.__dict__}\n has been sent to the Kitchen')
